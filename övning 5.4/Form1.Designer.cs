bfe808fc2626b8c15b12e18640ad1bc034a68cc0
viggo42 <viggo.ericsson.edu@rattvik.se>
2023-11-07 12:02:51 +0100

    Initial commit

---
+++ övning 5.4/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace övning_5._4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKör = new System.Windows.Forms.Button();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.lblSvar = new System.Windows.Forms.Label();
+            this.tbxA = new System.Windows.Forms.TextBox();
+            this.tbxB = new System.Windows.Forms.TextBox();
+            this.tbxC = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnKör
+            // 
+            this.btnKör.Location = new System.Drawing.Point(321, 336);
+            this.btnKör.Name = "btnKör";
+            this.btnKör.Size = new System.Drawing.Size(179, 23);
+            this.btnKör.TabIndex = 0;
+            this.btnKör.Text = "Kör";
+            this.btnKör.UseVisualStyleBackColor = true;
+            this.btnKör.Click += new System.EventHandler(this.btnKör_Click);
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(228, 143);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(32, 13);
+            this.lblA.TabIndex = 1;
+            this.lblA.Text = "Tal A";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(228, 179);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(32, 13);
+            this.lblB.TabIndex = 2;
+            this.lblB.Text = "Tal B";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(228, 224);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(32, 13);
+            this.lblC.TabIndex = 3;
+            this.lblC.Text = "Tal C";
+            // 
+            // lblSvar
+            // 
+            this.lblSvar.AutoSize = true;
+            this.lblSvar.Location = new System.Drawing.Point(399, 301);
+            this.lblSvar.Name = "lblSvar";
+            this.lblSvar.Size = new System.Drawing.Size(29, 13);
+            this.lblSvar.TabIndex = 4;
+            this.lblSvar.Text = "Svar";
+            // 
+            // tbxA
+            // 
+            this.tbxA.Location = new System.Drawing.Point(376, 143);
+            this.tbxA.Name = "tbxA";
+            this.tbxA.Size = new System.Drawing.Size(100, 20);
+            this.tbxA.TabIndex = 5;
+            // 
+            // tbxB
+            // 
+            this.tbxB.Location = new System.Drawing.Point(376, 179);
+            this.tbxB.Name = "tbxB";
+            this.tbxB.Size = new System.Drawing.Size(100, 20);
+            this.tbxB.TabIndex = 6;
+            // 
+            // tbxC
+            // 
+            this.tbxC.Location = new System.Drawing.Point(376, 221);
+            this.tbxC.Name = "tbxC";
+            this.tbxC.Size = new System.Drawing.Size(100, 20);
+            this.tbxC.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbxC);
+            this.Controls.Add(this.tbxB);
+            this.Controls.Add(this.tbxA);
+            this.Controls.Add(this.lblSvar);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.btnKör);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKör;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Label lblSvar;
+        private System.Windows.Forms.TextBox tbxA;
+        private System.Windows.Forms.TextBox tbxB;
+        private System.Windows.Forms.TextBox tbxC;
+    }
+}
+
---
+++ övning 5.4/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace övning_5._4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKör_Click(object sender, EventArgs e)
+        {
+            double tal1 = double.Parse(tbxA.Text); 
+            double tal2 = double.Parse(tbxB.Text);
+            double tal3 = double.Parse(tbxC.Text);
+            bool ordningsföljd = tal1 < tal2 && tal2 < tal3; // kollar om tal1 är minder än tal2
+            string info = " Talen är i ordningsföljd: " + ordningsföljd.ToString();
+            lblSvar.Text = info;   
+
+        }
+    }
+}
